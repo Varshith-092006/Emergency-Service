d472a896bebac071b1dfcdab63388fba9a305f1d
Varshtih <kollivarshith123@.com>
2025-06-27 20:39:29 +0530

    updated Config and Added .env file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,19 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
   },
-}) 
+  define: {
+    'process.env.API_URL': JSON.stringify(process.env.VITE_API_URL),
+  },
+});
